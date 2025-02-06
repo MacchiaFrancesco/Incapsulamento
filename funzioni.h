cc19bec32ed27caaa9809906a268bcdf9b8911ea
MacchiaFrancesco <francesco.macchia06@gmail.com>
2025-02-06 17:37:47 +0100

    Add files via upload

---
+++ funzioni.h
@@ -0,0 +1,13 @@
+#ifndef FUNZIONI_H
+#define FUNZIONI_H
+#include <string>
+using namespace std; //sconsigliato però non so farlo senza.
+
+void lettura();
+void scrittura(string messaggio);
+string charToBin(char c);		//prototipi delle funzioni che useremo
+string stringToBin(string &messaggio);
+void creaDatagram();
+void stampaHeader(creaDatagram());
+
+#endif
